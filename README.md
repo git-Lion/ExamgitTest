ff749ca3b222f6d96aa06825c104007ba3d80443
Laxman <Laxman@WINCTRL-KQB8VEI>
2019-05-26 09:42:48 +0530

    committed and changes done.

--- README.md
+++
@@ -1,2 +0,0 @@
-# ExamgitTest
-fdsfdfdfdfd sdfd fsdfsdf
---
+++ TestGit/src/ddd/wewe.java
@@ -0,0 +1,13 @@
+package ddd;
+
+public class wewe {
+
+	public static void main(String[] args) {
+		System.out.println("git test fgf gfgfdssfs df");
+		
+		System.out.println("hi git");
+		
+		
+	}
+
+}
